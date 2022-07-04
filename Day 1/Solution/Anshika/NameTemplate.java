b3d991a49d724582a807eb48fbfcf9a53b9512a9
Subham Maity <97989643+Subham-Maity@users.noreply.github.com>
2022-07-04 22:31:37 +0530

    NameTemplate anshika

---
+++ Day 1/Solution/Anshika/NameTemplate.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class nameTemplate{
+public static void main(String[] args){
+Scanner scan = new Scanner(System.in);
+System.out.println("Enter the name");
+String name = scan.next();
+System.out.println("Hello "+ name +" have a good day." );
+
+  }
+}
