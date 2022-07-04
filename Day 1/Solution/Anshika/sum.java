09f6976be4f7dccb69b8b4eafbed543cfc62c6e8
Subham Maity <97989643+Subham-Maity@users.noreply.github.com>
2022-07-04 21:41:54 +0530

    Anshika sum solution .

---
+++ Day 1/Solution/Anshika/sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class abs1{
+public static void main(String[] args){
+int a,b,c,sum;
+Scanner scan = new Scanner(System.in);
+System.out.println("Enter the first number");
+int x = scan.nextInt();
+System.out.println("Enter the second number");
+int y = scan.nextInt();
+System.out.println("Enter the third number");
+int z = scan.nextInt();
+sum = (x+y+z);
+System.out.println("sum");
+System.out.println(sum);
+}
+}
